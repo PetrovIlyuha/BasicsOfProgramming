c14e1d6d914ab1e619e48af04d4873ec4310caa3
Ilya Petrov <dnamix1@gmail.com>
2020-01-14 20:39:57 +0300

    last part of FP in JS transcribed

--- functionalJS.js
+++ functionalJS.js
@@ -28,7 +28,14 @@
  * Ветвление ИП - условные операторы  \ ФП - используем только условные выражения (ternary operator)
  * Итерирование ИП - обычные итерирующие методы \
  * ФП - рекурсия - ничего не может исполнятся несколько раз в одном функциональном контексте
- *
+ * Контекст - ИП - объекты, на которых построено состояние подвергается мутациям (вызов АПИ или изменения в структурах данных)
+ * Контекст - ФП - в качестве контекста выступает замыкание (замыкание - функторы - монады)
+ * Пулы в ФП не могут присутствовать - но пулификацию фабрик можно делать
+ * ИП - наследование на классах и прототипах \ ФП - за счёт частичного применения, каррирования и композиции - снижаем \ повышаем уровень абстракции
+ * Композиция повышает уровень абстракции, а частичное применение - понижает
+ * Боксирование - ИП = Фасад и Адаптер \ ФП = врапперы и замыкания
+ * Расширение - ИП = миксины и декораторы \ ФП = Map и Декораторы
+ * Кеширование - ИП = на структурах данных B-tree, hash-table / ФП = Функциональный контекст как основа мемоизации
  *
  *
  */
@@ -138,3 +145,99 @@ iterate(numbers, (n, i) => {
 numbers.forEach((n, i) => {
   console.log(`Item ${i}  is ${n}`);
 });
+
+// ! Instantiation
+// closure context instead of object context
+// Imperative
+const model = {
+  name: "BMW Series 5",
+  year: "2018",
+  upperName() {
+    this.name = this.name.toUpperCase();
+  },
+  get age() {
+    return new Date().getFullYear() - this.year;
+  },
+  toString() {
+    return `${this.name} was made in ${this.year}`;
+  }
+};
+
+// FP style
+const age = year => new Date().getFullYear() - year;
+const modelFP = (name, year) => () =>
+  age(year) > 1
+    ? `${name} was made ${age(year)} years ago}`
+    : `${name} was made ${age(year)} year ago}`;
+const bmw7 = modelFP("BMW Series 7", 2019);
+
+// Map object instead of key iteration
+// Imperative
+const merkel = {
+  firstname: "Angela",
+  lastname: "Merkel",
+  role: "bundeskanzler"
+};
+
+const politician = {};
+
+for (const key in merkel) {
+  const prop = key.toLowerCase().replace("name", "");
+  const value = merkel[key].toUpperCase();
+  politician[prop] = value;
+}
+
+console.log(politician);
+
+// Functional
+
+const inst = (prev, prop, val) => ({ ...prev, [prop]: val });
+
+const omap = (obj, fn) =>
+  Object.keys(obj).reduce((prev, key) => inst(prev, ...fn(key, obj[key]), {}));
+
+const bernanke = {
+  firstname: "Ben",
+  lastname: "Bernanke",
+  jobtitle: "banker"
+};
+
+const bernanko = omap(bernanke, (key, val) => [
+  key.toLowerCase().replace("name", ""),
+  val.toUpperCase()
+]);
+
+console.log(bernanko);
+
+// Chaining IP
+class Adder {
+  constructor(initial) {
+    this.value = initial;
+  }
+  add(value) {
+    this.value += value;
+    return this;
+  }
+  valueOf() {
+    return this.value;
+  }
+}
+
+const sum1 = new Adder(1).add(9).add(10);
+console.log(+sum1);
+
+// Chaining FP
+const adder = initial =>
+  Object.assign(value => adder(initial + value), { valueOf: () => initial });
+
+const sum2 = adder(1)(20)(101);
+console.log(+sum2);
+
+// FP with methods
+const add = initial => ({
+  add: value => add(initial + value),
+  valueOf: () => initial
+});
+
+const sum3 = add(9).add(10);
+console.log(+sum3);
