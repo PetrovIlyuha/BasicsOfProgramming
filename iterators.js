633be32405ea0f4ba4abdb7b409f8e2f27800235
Ilya Petrov <dnamix1@gmail.com>
2020-02-24 10:35:20 +0300

    iterators in JS

--- iterators.js
+++ iterators.js
@@ -71,86 +71,114 @@ const numbers = [1, 2, 3, 4, 5];
 // console.log(result1);
 
 // ? matrix
-const matrix = [
-  [2, 45, 36, 200, 1],
-  [4, 65, 100, 87],
-  [2, 34, 12, 300, 4],
-  [54, 66, 78, 400, 43]
-];
+// const matrix = [
+//   [2, 45, 36, 200, 1],
+//   [4, 65, 100, 87],
+//   [2, 34, 12, 300, 4],
+//   [54, 66, 78, 400, 43]
+// ];
 
-const max = (a, b) => (a > b ? a : b);
+// const max = (a, b) => (a > b ? a : b);
 
-const result = matrix
-  .map(row => row.reduce(max))
-  .reduce((acc, rowMax) => acc + rowMax);
+// const result = matrix
+//   .map(row => row.reduce(max))
+//   .reduce((acc, rowMax) => acc + rowMax);
 
-console.log(result);
+// console.log(result);
 
-for (const i in matrix) {
-  const row = matrix[i];
-  for (const j in row) {
-    const col = row[j];
-    console.log(i, j, col);
-  }
-}
+// for (const i in matrix) {
+//   const row = matrix[i];
+//   for (const j in row) {
+//     const col = row[j];
+//     console.log(i, j, col);
+//   }
+// }
 
-for (const row of matrix) {
-  for (const item of row) {
-    console.log(item);
-  }
-}
+// for (const row of matrix) {
+//   for (const item of row) {
+//     console.log(item);
+//   }
+// }
 
 // the way forEach works we automatically gets indexes
-matrix.forEach((row, i) => {
-  row.forEach((col, j) => {
-    console.log(i, j, col);
-  });
-});
+// matrix.forEach((row, i) => {
+//   row.forEach((col, j) => {
+//     console.log(i, j, col);
+//   });
+// });
 
-// iterator
+// // iterator
+// const range = {
+//   start: 1,
+//   end: 10,
+//   [Symbol.iterator]() {
+//     let value = this.start;
+//     return {
+//       next: () => ({
+//         value,
+//         done: value++ === this.end + 1
+//       })
+//     };
+//   }
+// };
+
+// console.dir({
+//   range,
+//   names: Object.getOwnPropertyNames(range),
+//   symbols: Object.getOwnPropertySymbols(range)
+// });
+
+// for (const number of range) {
+//   console.log(number);
+// }
+
+// const sum = (prev, current) => prev + current;
+// const sumIterable = (...iterable) => iterable.reduce(sum);
+
+// const sumOfRange = sumIterable(...range);
+// console.log(`Sum of Range = ${sumOfRange}`);
+
+// reverse-iteration
+// const arr = [20, 15, 10, 5, 1];
+
+// arr[Symbol.iterator] = function() {
+//   let index = this.length;
+//   return {
+//     next: () => ({
+//       done: index-- === 0,
+//       value: this[index]
+//     })
+//   };
+// };
+
+// for (const num in arr) {
+//   console.log(arr[num]);
+// }
+
+// async-iterator
 const range = {
   start: 1,
   end: 10,
-  [Symbol.iterator]() {
+  [Symbol.asyncIterator]() {
     let value = this.start;
     return {
-      next: () => ({
-        value,
-        done: value++ === this.end + 1
-      })
+      next: () =>
+        Promise.resolve({
+          value,
+          done: ++value === this.end
+        })
     };
   }
 };
 
-console.dir({
+console.log({
   range,
   names: Object.getOwnPropertyNames(range),
   symbols: Object.getOwnPropertySymbols(range)
 });
 
-for (const number of range) {
-  console.log(number);
-}
-
-const sum = (prev, current) => prev + current;
-const sumIterable = (...iterable) => iterable.reduce(sum);
-
-const sumOfRange = sumIterable(...range);
-console.log(`Sum of Range = ${sumOfRange}`);
-
-// reverse-iteration
-const arr = [5, 4, 3, 2, 1];
-
-arr[Symbol.iterator] = function() {
-  let index = this.length;
-  return {
-    next: () => ({
-      done: index-- === 0,
-      value: this[index]
-    })
-  };
-};
-
-for (const num in arr) {
-  console.log(num);
-}
+(async () => {
+  for await (const number of range) {
+    console.log(number);
+  }
+})();
